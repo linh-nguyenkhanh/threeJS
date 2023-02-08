4553d1944518ffe2af9830563a665c5cc8b065c0
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-02-08 18:47:08 +0200

    resize window and camera

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 import * as THREE from "three";
 
+import "./styles.css";
 // scene
 const scene = new THREE.Scene();
 
 // create sphere
 const geometryShape = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
+  color: "#FFC0CB",
 });
 const mesh = new THREE.Mesh(geometryShape, material);
 scene.add(mesh);
 
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// add light on black screen
+const light = new THREE.PointLight(0xffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
 // camera
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+// z index to move back camera
+camera.position.z = 20;
 scene.add(camera);
+
+// render scene
+const canvas = document.querySelector(".webgl");
+const renderScene = new THREE.WebGLRenderer({ canvas });
+
+renderScene.setSize(sizes.width, sizes.height);
+renderScene.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  // resize window
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // update camera
+  camera.aspect = sizes.width / sizes.height;
+});
